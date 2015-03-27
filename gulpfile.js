bc0e3bb95270d70e2d73c656b0a7f5289edf699c
ryane <ryanesc@gmail.com>
2015-03-27 09:02:19 -0400

    - integrated hugo into gulp default task - improved clean - added css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    del = require('del');
+    minifyCss = require('gulp-minify-css'),
+    del = require('del'),
+    cp = require('child_process');
 
-gulp.task('clean', function(done) {
-  cleanDirs = ['public', 'static'];
-  del(cleanDirs, { force: true }, done);
+gulp.task('clean', function() {
+  cleanDirs = ['public', 'static/**/*'];
+  del(cleanDirs, { force: true });
 });
 
 gulp.task('scss', function() {
   gulp.src('src/scss/*')
     .pipe(sass())
+    .pipe(minifyCss())
     .pipe(gulp.dest('./static/css/'));
 });
 
-gulp.task('default', [ 'clean', 'scss']);
+gulp.task('watch', ['scss'], function(done) {
+  gulp.watch(['src/css/*'], ['scss']);
+  var hugoArgs = [
+    'server',
+    '-w',
+    '--buildDrafts',
+    '-t',
+    'ryaneschinger.com'
+  ];
+  return cp
+    .spawn('hugo', hugoArgs, { stdio: 'inherit' })
+    .on('close', done);
+});
+
+gulp.task('default', [ 'watch' ]);
