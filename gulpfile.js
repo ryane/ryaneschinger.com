a6bc54dd32d7550ca403607209bbac2107736701
ryane <ryanesc@gmail.com>
2015-03-27 07:12:57 -0400

    added gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    del = require('del');
+
+gulp.task('clean', function(done) {
+  cleanDirs = ['public', 'static'];
+  del(cleanDirs, { force: true }, done);
+});
 
 gulp.task('scss', function() {
   gulp.src('src/scss/*')
@@ -7,4 +13,4 @@ gulp.task('scss', function() {
     .pipe(gulp.dest('./static/css/'));
 });
 
-gulp.task('default', ['scss']);
+gulp.task('default', [ 'clean', 'scss']);
