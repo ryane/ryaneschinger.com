1091686dc24911ec6085bc5cb90746fe48a4d933
ryane <ryanesc@gmail.com>
2020-07-10 15:42:31 -0400

    reset

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var Hello = require('./app/hello.jsx');
-
-jQuery( document ).ready(function() {
-  if (document.getElementById('app'))
-    React.render(<Hello />, document.getElementById('app'));
-});
